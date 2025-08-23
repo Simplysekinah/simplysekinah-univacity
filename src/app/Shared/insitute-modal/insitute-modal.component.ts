1a08343d2a88ea74ae968342146fd214b0f4585f
Simplysekinah <akinolasekinat123@gmail.com>
2025-08-23 22:14:25 +0100

    country search

--- src/app/Shared/insitute-modal/insitute-modal.component.ts
+++ src/app/Shared/insitute-modal/insitute-modal.component.ts
@@ -8,36 +8,43 @@ import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-insitute-modal',
-  imports: [IonicModule,CommonModule,FormsModule],
+  imports: [IonicModule, CommonModule, FormsModule],
   templateUrl: './insitute-modal.component.html',
   styleUrl: './insitute-modal.component.css'
 })
 export class InsituteModalComponent {
 
+  programs!: Program[];
   filteredPrograms: Program[] = [];
-    programs!: Program[];
+  searchTerm = '';
+  selectedId: number | null = 2;
+  institutes = [
+    { id: 1, name: 'University of Graz', country: 'Canada', count: 629, logo: 'top-image.png' },
+    { id: 2, name: 'University of Liverpool', country: 'Canada', count: 629, logo: 'top-image.png' },
+    { id: 3, name: 'University of Liverpool', country: 'Canada', count: 629, logo: 'top-image.png' },
+    { id: 4, name: 'University of Liverpool', country: 'Canada', count: 629, logo: 'top-image.png' },
+    { id: 5, name: 'University of Liverpool', country: 'Canada', count: 629, logo: 'top-image.png' },
+    { id: 6, name: 'University of Liverpool', country: 'Canada', count: 629, logo: 'top-image.png' },
+  ];
 
-    constructor(
+
+  constructor(
     private modalCtrl: ModalController,
     private programService: ProgramService,
     private router: Router
-  ) {}
-      ngOnInit() {
+  ) { }
+  ngOnInit() {
     this.programs = this.programService.getPrograms();
     this.filteredPrograms = [...this.programs];
   }
 
-
-  searchTerm = '';
-  selectedId: number | null = 2; // Example pre-selected item
-  institutes = [
-    { id: 1, name: 'University of Graz', country: 'Canada', count: 629, logo: 'assets/university-logo.png' },
-    { id: 2, name: 'University of Liverpool', country: 'Canada', count: 629, logo: 'assets/university-logo.png' },
-    // ... more items
-  ];
-
-
-  close() { this.modalCtrl.dismiss(); }
-  clearAll() { this.selectedId = null; }
-  apply() { this.modalCtrl.dismiss({ selected: this.selectedId }); }
+  close() { 
+    this.modalCtrl.dismiss(); 
+  }
+  clearAll() { 
+    this.selectedId = null; 
+  }
+  apply() { 
+    this.modalCtrl.dismiss({ selected: this.selectedId });
+  }
 }
